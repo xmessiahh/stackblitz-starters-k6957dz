e32ecd2d67855d49e7fe2918bf6766569bcec073
xmessiahh <kennethoreo24@gmail.com>
2024-11-14 12:36:59 -0500

    java practice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello!');
+
+const todaysDate = () => {
+   let d = new Date();
+   return "Today's date is" + d
+}
+
+const dateBlock = document.getElementById("dateBlock");
+dateBlock.innerText = todaysDate();
+const body = document.body;
+const d = new Date();
+if (d.getHours() > 20 || d.getHours() < 9 ){
+    body.style["backgroundColor"] = "green";
+    body.style["color"] = "darkgreen";
+} else {
+    body.style["backgroundColor"] = "darkgreen";
+    body.sty;e["color"] = "green";
+}       
