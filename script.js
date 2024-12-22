76f1190ba4d903c7f2c973f0ca0a794aa563b238
xmessiahh <kennethoreo24@gmail.com>
2024-12-21 22:32:17 -0500

    dateblock

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ console.log('Hello!');
 
 const todaysDate = () => {
    let d = new Date();
-   return "Today's date is" + d
+   return "Today's date is " + d
 }
 
 const dateBlock = document.getElementById("dateBlock");
 dateBlock.innerText = todaysDate();
 const body = document.body;
 const d = new Date();
+
 if (d.getHours() > 20 || d.getHours() < 9 ){
     body.style["backgroundColor"] = "white";
     body.style["color"] = "black";
